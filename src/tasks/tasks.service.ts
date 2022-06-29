1ab41120fb5de552c151e575d0c24247e92ff47e
buitrago <buitrago@imagine-computation.com>
2022-06-30 00:37:39 +0200

    init

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  public getAllTasks() {
+    return this.tasks;
+  }
+
+  public createTasks(): Task {
+    
+  }
+}
